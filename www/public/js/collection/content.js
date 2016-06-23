aa54007ba40ddbe64e83a989942f4a555336130b
Peter Seer <pseer@psx>
2016-06-23 15:46:43 +0200

    removed some files we don't need

--- www/public/js/collection/content.js
+++
@@ -1,5 +0,0 @@
-define(['model/content-entry'], function(Content) {
-	return Backbone.Collection.extend({
-		model: Content
-	});
-});
--- www/public/js/model/content-entry.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-	return Backbone.Model.extend({
-		defaults: {
-			id: 'Not specified',
-			city: 'Not specified',
-			type: 'Not specified'
-		}
-	});
-});
